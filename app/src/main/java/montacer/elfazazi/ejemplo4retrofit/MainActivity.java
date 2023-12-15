b137f125b698d457e66c6a116282e5d3306b7d59
MontacerElFazazi <montacer.elfazazielfazazi@cieep.com>
2023-12-15 09:29:50 +0100

    Initial commit

---
+++ app/src/main/java/montacer/elfazazi/ejemplo4retrofit/MainActivity.java
@@ -0,0 +1,31 @@
+package montacer.elfazazi.ejemplo4retrofit;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+
+import montacer.elfazazi.ejemplo4retrofit.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
